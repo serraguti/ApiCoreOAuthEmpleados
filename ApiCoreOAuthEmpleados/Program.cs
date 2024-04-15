1c5a15a6adefe3236f0ef2a27d9bcb1d77724a6d
serraguti <pacoserranox@gmail.com>
2024-04-15 09:34:58 +0200

    commit

--- ApiCoreOAuthEmpleados/Program.cs
+++ ApiCoreOAuthEmpleados/Program.cs
@@ -3,6 +3,8 @@ using ApiCoreOAuthEmpleados.Helpers;
 using ApiCoreOAuthEmpleados.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
+using NSwag.Generation.Processors.Security;
+using NSwag;
 
 var builder = WebApplication.CreateBuilder(args);
 //CREAMOS UNA INSTANCIA DEL HELPER
@@ -28,19 +30,41 @@ builder.Services.AddDbContext<HospitalContext>
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(options =>
+// REGISTRAMOS SWAGGER COMO SERVICIO
+builder.Services.AddOpenApiDocument(document =>
 {
-    options.SwaggerDoc("v1", new OpenApiInfo
-    {
-        Title = "Api OAuth Empleados",
-        Description = "Api con Token de seguridad"
-    });
+    document.Title = "Api OAuth Empleados";
+    document.Description = "Api con seguridad 2024";
+    // CONFIGURAMOS LA SEGURIDAD JWT PARA SWAGGER,
+    // PERMITE AÑADIR EL TOKEN JWT A LA CABECERA.
+    document.AddSecurity("JWT", Enumerable.Empty<string>(),
+        new NSwag.OpenApiSecurityScheme
+        {
+            Type = OpenApiSecuritySchemeType.ApiKey,
+            Name = "Authorization",
+            In = OpenApiSecurityApiKeyLocation.Header,
+            Description = "Copia y pega el Token en el campo 'Value:' así: Bearer {Token JWT}."
+        }
+    );
+    document.OperationProcessors.Add(
+    new AspNetCoreOperationSecurityScopeProcessor("JWT"));
 });
+//builder.Services.AddSwaggerGen(options =>
+//{
+//    options.SwaggerDoc("v1", new OpenApiInfo
+//    {
+//        Title = "Api OAuth Empleados",
+//        Description = "Api con Token de seguridad"
+//    });
+//});
 
 var app = builder.Build();
-app.UseSwagger();
+app.UseOpenApi();
+//app.UseSwagger();
 app.UseSwaggerUI(options =>
 {
+    options.InjectStylesheet("/css/monokai_theme.css");
+    //options.InjectStylesheet("/css/material3x.css");
     options.SwaggerEndpoint(url: "/swagger/v1/swagger.json"
         , name: "Api OAuth Empleados");
     options.RoutePrefix = "";
@@ -52,7 +76,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseStaticFiles();
 app.UseAuthentication();
 app.UseAuthorization();
 
